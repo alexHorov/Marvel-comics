552e4ba127197613c213ae950d858a0a9eea2ba5
Alexandr Horovyi <alexhorovyi@gmail.com>
2023-03-06 01:03:35 +0200

    use Helmeet

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,17 @@
 import ErrorMessage from "../errorMessage/ErrorMessage";
+import { Helmet } from 'react-helmet';
 import { Link } from "react-router-dom";
 
 const Page404 = () => {
     return (
         <div>
+            <Helmet>
+                <meta
+                    name="description"
+                    content='Page404'
+                />
+                <title>Page404</title>
+            </Helmet>
             <ErrorMessage />
             <p style={{ 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px' }}>Page doesn't exist</p>
             <Link style={{ 'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px' }} to="/">Back to main page</Link>
--- src/components/pages/singleComicLayout/SingleComicLayout.js
+++ src/components/pages/singleComicLayout/SingleComicLayout.js
@@ -1,5 +1,6 @@
-import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import { Link } from 'react-router-dom';
 import './singleComicLayout.scss';
 
 const SingleComicLayout = ({ data }) => {
@@ -7,26 +8,33 @@ const SingleComicLayout = ({ data }) => {
     const { title, description, pageCount, thumbnail, language, price } = data;
 
     return (
-        <SwitchTransition>
-            <CSSTransition
-                key={title}
-                timeout={500}
-                // in={true}
-                classNames="single-comic"
-                unmountOnExit
-            >
-                <div className="single-comic">
-                    <img src={thumbnail} alt={title} className="single-comic__img" />
-                    <div className="single-comic__info">
-                        <h2 className="single-comic__name">{title}</h2>
-                        <p className="single-comic__descr">{description}</p>
-                        <p className="single-comic__descr">{pageCount}</p>
-                        <p className="single-comic__descr">Language: {language}</p>
-                        <div className="single-comic__price">{price}</div>
-                    </div>
-                    <Link to="/comics" className="single-comic__back">Back to all</Link>
+        // <SwitchTransition>
+        <CSSTransition
+            // key={title}
+            timeout={500}
+            // in={true}
+            classNames="single-comic"
+        // unmountOnExit
+        >
+            <div className="single-comic">
+                <Helmet>
+                    <meta
+                        name="description"
+                        content={`${title} comics book`}
+                    />
+                    <title>{title}</title>
+                </Helmet>
+                <img src={thumbnail} alt={title} className="single-comic__img" />
+                <div className="single-comic__info">
+                    <h2 className="single-comic__name">{title}</h2>
+                    <p className="single-comic__descr">{description}</p>
+                    <p className="single-comic__descr">{pageCount}</p>
+                    <p className="single-comic__descr">Language: {language}</p>
+                    <div className="single-comic__price">{price}</div>
                 </div>
-            </CSSTransition>
+                <Link to="/comics" className="single-comic__back">Back to all</Link>
+            </div>
+        </CSSTransition>
         // </SwitchTransition>
 
     )
